34cc0b91927eb00fecdaa75316254c110abdb6a8
Misbah Imtiaz <misb2001@gmail.com>
2023-01-12 04:15:32 -0600

    removed uneccessary files, added new components, and removed previous animations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './sections/Greeting';
+import Intro from './sections/Intro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting/>
+      <Intro/>
     </div>
   );
 }
+
 
 export default App;
