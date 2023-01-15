b665b8e53421114e52f67b7438843d94e709b3b5
Misbah Imtiaz <misb2001@gmail.com>
2023-01-15 03:22:33 -0600

    added gradient to project section, fixed nordstrom bug

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <section>Projects</section>
+    <section className='projects'>Projects</section>
   )
 }
 
--- src/sections/Work.js
+++ src/sections/Work.js
@@ -32,7 +32,7 @@ const nordstrom = {
     term: "spring 2023",
     work: "working on a data analytics project that deals with displaying sales information to employees.",
     skills: "tbd",
-    image: Nordstrom
+    image: Gray_Nord
 }
 const atlassian = {
     name: "atlassian",
@@ -48,80 +48,67 @@ let index = 0;
 const workplaces = [paycom, ibm, nordstrom, atlassian]
 
 const Work = () => {
-    const [isJob, setJob] = useState(paycom);  
-    const [paycomGray, setPaycomGray] = useState("grayscale(0)"); 
-    const [ibmGray, setibmGray] = useState("grayscale(100%)"); 
-    const [atlGray, setatlGray] = useState("grayscale(100%)");  
-    
-    const handleClick = (company)  => {
+    const [isJob, setJob] = useState(paycom);
+    const [paycomGray, setPaycomGray] = useState("grayscale(0)");
+    const [ibmGray, setibmGray] = useState("grayscale(100%)");
+    const [atlGray, setatlGray] = useState("grayscale(100%)");
+
+    const handleClick = (company) => {
         if (company == paycom) {
             setJob(paycom);
             setPaycomGray("grayscale(0)");
-            setatlGray("grayscale(100%)"); 
-            setibmGray("grayscale(100%)"); 
-            nordstrom.image = Gray_Nord; 
+            setatlGray("grayscale(100%)");
+            setibmGray("grayscale(100%)");
+            nordstrom.image = Gray_Nord;
         } else if (company == ibm) {
             setJob(ibm);
             setibmGray("grayscale(0)");
-            setatlGray("grayscale(100%)"); 
-            setPaycomGray("grayscale(100%)"); 
-            nordstrom.image = Gray_Nord; 
+            setatlGray("grayscale(100%)");
+            setPaycomGray("grayscale(100%)");
+            nordstrom.image = Gray_Nord;
         } else if (company == atlassian) {
             setJob(atlassian);
             setatlGray("grayscale(0)");
-            setibmGray("grayscale(100%)"); 
-            setPaycomGray("grayscale(100%)"); 
-            nordstrom.image = Gray_Nord; 
+            setibmGray("grayscale(100%)");
+            setPaycomGray("grayscale(100%)");
+            nordstrom.image = Gray_Nord;
         } else {
             setJob(nordstrom);
             setatlGray("grayscale(100%)");
-            setibmGray("grayscale(100%)"); 
-            setPaycomGray("grayscale(100%)"); 
-            nordstrom.image = Nordstrom; 
+            setibmGray("grayscale(100%)");
+            setPaycomGray("grayscale(100%)");
+            nordstrom.image = Nordstrom;
         }
     }
 
-    
-
-    // const [nordGray, setnordGray] = useState("grayscale(100%)"); 
-
-
     return (
         <section className="work">
-            <div style={{display: "grid"}}>
-                {/* {workplaces.map(company => <WorkCard className="top-level" key={index++} name={company.name} title={company.title} term={company.term} 
-                    work={company.work} skills={company.skills} image={company.image} show={false} />)} */}
+            <div style={{ display: "grid" }}>
                 <div className="title">
                     <p>where have i worked?</p>
                 </div>
 
-                <div style={{display: "grid", justifyContent:"center", alignItems: "center", gridTemplateColumns: "50% 50%", columnGap:"10%"}}>
-                <div className='center'>
-                    <div>
-                        <span><img src={paycom.image} alt={"paycom"} style={{filter: paycomGray}} onClick={() =>{handleClick(paycom)}}/> </span>
-                    </div>
-                    <div>
-                        <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
-                        <span><img className = "logo" src={ibm.image} alt={"ibm"} style={{margin: "40px", filter: ibmGray }} onClick={() =>{handleClick(ibm)}}/> </span>
-                        <span style={{marginBottom: "10px"}}>click me!</span>
-                        <span><img className = "logo" src={atlassian.image} alt={"atlassian"} style={{margin: "40px", filter: atlGray }} onClick={() =>{handleClick(atlassian)}}/> </span>
+                <div style={{ display: "grid", justifyContent: "center", alignItems: "center", gridTemplateColumns: "50% 50%", columnGap: "10%" }}>
+                    <div className='center'>
+                        <div>
+                            <span><img src={paycom.image} alt={"paycom"} style={{ filter: paycomGray }} onClick={() => { handleClick(paycom) }} /> </span>
+                        </div>
+                        <div>
+                            <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
+                                <span><img className="logo" src={ibm.image} alt={"ibm"} style={{ margin: "40px", filter: ibmGray }} onClick={() => { handleClick(ibm) }} /> </span>
+                                <span style={{ marginBottom: "10px" }}>click me!</span>
+                                <span><img className="logo" src={atlassian.image} alt={"atlassian"} style={{ margin: "40px", filter: atlGray }} onClick={() => { handleClick(atlassian) }} /> </span>
+                            </div>
+                        </div>
+                        <div>
+                            <span><img className="logo" src={nordstrom.image} alt={"nordstrom"} onClick={() => { handleClick(nordstrom) }} /></span>
                         </div>
                     </div>
-                    <div>
-                        <span><img className = "logo" src={nordstrom.image} alt={"nordstrom"} onClick={() =>{handleClick(nordstrom)}}/></span>
+                    <div className='card-container'>
+                        <WorkCard key={0} name={isJob.name} title={isJob.title} term={isJob.term}
+                            work={isJob.work} skills={isJob.skills} image={isJob.image} show={true}></WorkCard>
                     </div>
                 </div>
-                <div className='card-container'>
-                    <WorkCard key={0} name={isJob.name} title={isJob.title} term={isJob.term} 
-                    work={isJob.work} skills={isJob.skills} image={isJob.image} show={true}></WorkCard>
-                </div>
-                
-
-                </div>
-               
-
-                
-
             </div>
         </section>
     )
