f592b36fe1f4d769c72f0aeb068f8ff5a1f44c61
Misbah Imtiaz <misb2001@gmail.com>
2023-01-31 13:54:19 -0600

    langauges section completed i think, animations and mobile screen optimization

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion} from "framer-motion";
 
 function Item({item}) {
     return <li style={{marginTop:"4px"}}>{item}</li>;
@@ -8,14 +9,14 @@ let langKey = 100;
 let frameKey = 200;
 let toolKey = 300; 
 
-function setPos(val) {
-    if (val === 100) {
-        return "left"; 
-    } else if (val === 200){
-        return "center";
-    }
-    return "right";
-}
+// function setPos(val) {
+//     if (val === 100) {
+//         return "left"; 
+//     } else if (val === 200){
+//         return "center";
+//     }
+//     return "right";
+// }
 
 
 function List({list, key}) {
@@ -24,25 +25,69 @@ function List({list, key}) {
     </ul>; 
 }
 
+const sect = {
+    hidden: { scale: 0 },
+    show: {
+        scale: 1,
+        transition: {
+            type: "spring",
+        }
+    },
+}
+
+const sect2 = {
+    hidden: { scale: 0 },
+    show: {
+        scale: 1,
+        transition: {
+            delay : .25,
+            type: "spring",
+        }
+    },
+}
+
+const sect3 = {
+    hidden: { scale: 0 },
+    show: {
+        scale: 1,
+        transition: {
+            delay : .5, 
+            type: "spring",
+        }
+    },
+}
 
 
-const SkillsList = ({languages, frameworks, tools}) => {
+
+const SkillsList = ({languages, frameworks, tools, isVisible}) => {
   return (
     <div className='grid-container'>
-        <div className='item' style={{textAlign: setPos(langKey)}}>
+        <motion.div className='item' style={{textAlign: "left"}}
+            variants={sect} 
+            initial='hidden'
+            animate={isVisible ? "show" : "hidden"}
+        >
             <p>languages</p>
             <List list={languages} key={langKey}/>
-        </div>
+        </motion.div>
 
-        <div className='item' style={{textAlign: setPos(frameKey)}}>
+        <motion.div className='item' style={{textAlign: "center"}}
+            variants={sect3} 
+            initial='hidden'
+            animate={isVisible ? "show" : "hidden"}
+        >
             <p>frameworks and libraries</p>
             <List list={frameworks} key={frameKey}/>
-        </div>
+        </motion.div>
 
-        <div className='item' style={{textAlign: setPos(toolKey)}}>
+        <motion.div className='item' style={{textAlign: "right"}}
+            variants={sect2} 
+            initial='hidden'
+            animate={isVisible ? "show" : "hidden"}
+        >
             <p>tools</p>
             <List list={tools} key={toolKey}/>
-        </div>
+        </motion.div>
     </div>
   )
 }
--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import SkillsList from '../components/SkillsList'
 import down from '../images/bottom-arrow.png'
-
+import { useInView } from 'react-intersection-observer';
 
 const languages = ["java", "python", "c, c++", "golang", "javascript", "kotlin", "swift", "sql", "html, css"];
 const frameworks = ["flask", "gin", "react", "node", "express", "beego", "opencv", "pandas, matplotlib, numpy", "scikit-learn", "javafx"];
 const tools = ["git", "aws s3, rds", "postgresql", "mysql", "firebase", "jupiter notebook", "postman", "swagger", "heroku", "netlify"];
 
 const Skills = () => {
+    const {ref: myRef, inView: isVisible} = useInView();
   return (
     <section id="skills-page" className="languages">
         {/* <p className="title" style={{fontSize:"33px"}}>what do i know?</p> */}
-        <SkillsList className="center" languages={languages} frameworks={frameworks} tools={tools}/>
+        <div ref = {myRef} style={{position:"absolute", top:"50%", left:"50%"}}></div>
+        <SkillsList className="center" languages={languages} frameworks={frameworks} tools={tools} isVisible={isVisible}/>
         <a href='#projects-page' className='bot-btn' style={{width:"30px", height:"30px"}}>
             <img src={down} alt="bottom" style={{width:"30px", height:"30px"}}></img>
         </a>
