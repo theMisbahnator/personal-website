10418eca9fe2fd18adab133ba42bc0ed7a4e6c9f
Misbah Imtiaz <misb2001@gmail.com>
2024-10-06 01:55:07 -0700

    remove warnings

--- src/sections/Greeting.js
+++ src/sections/Greeting.js
@@ -1,18 +1,20 @@
 import {React} from 'react'
-import WavyText from '../components/WavyText'
-import { useInView } from 'react-intersection-observer';
-import DownButton from '../components/DownButton';
+// import WavyText from '../components/WavyText'
+// import { useInView } from 'react-intersection-observer';
+// import DownButton from '../components/DownButton';
 
 const Greeting = () => {
-  const {ref: myRef, inView: isVisible} = useInView();
+  // const {ref: myRef, inView: isVisible} = useInView();
 
   return (
     <section id="greeting-page" className="greeting">
-      <div ref = {myRef} >
+      {/* <div ref = {myRef} >
         {/* <WavyText id="greeting-text" text="hello!" replay={isVisible} cName="greeting"/>
         <DownButton page ='#intro-page' /> */}
-        <p>offline</p>
-      </div>
+        
+     {/* </div> */} 
+
+    <p>offline</p>
     </section>
     
   )
